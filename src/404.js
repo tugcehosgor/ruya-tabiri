f4beef8cbd5e46802cc722e9f451ec9f4267a425
tugcehosgor <tugcehosgor@gmail.com>
2021-12-12 21:53:00 -0500

    get update

--- src/404.js
+++ src/404.js
@@ -1,34 +1,36 @@
 var currentUrl = window.location.pathname
 console.log(currentUrl)
+var key = get(ref(database,"heteLEme"))
+console.log("keys:" + key)
 
-if (currentUrl === get(ref(database, ruyaURL),ruya)){
+// if (currentUrl === get(ref(database, ruyaURL),ruya)){
 
-    document.getElementById("404html").innerHTML = `
-  <div> 
-<row>
-  <col></col>
-  <col></col>
-  <col>
-    <label>Ruyada</label><br>
-    <label>${ruya}</label><br>
-    <label >gormeniz</label><br>
-    <label >Kicinizin acik kaldigina delalet etmektedir.</label><br>
+//     document.getElementById("404html").innerHTML = `
+//   <div> 
+// <row>
+//   <col></col>
+//   <col></col>
+//   <col>
+//     <label>Ruyada</label><br>
+//     <label>${ruya}</label><br>
+//     <label >gormeniz</label><br>
+//     <label >Kicinizin acik kaldigina delalet etmektedir.</label><br>
     
     
-`;
-}
+// `;
+// }
 
-else {
-    document.getElementById("404html").innerHTML = `
-  <div> 
-<row>
-  <col></col>
-  <col></col>
-  <col>
-    <label>Ruyaniz hayrolsun.</label><br>
+// else {
+//     document.getElementById("404html").innerHTML = `
+//   <div> 
+// <row>
+//   <col></col>
+//   <col></col>
+//   <col>
+//     <label>Ruyaniz hayrolsun.</label><br>
     
     
     
     
-`;
-}
+// `;
+// }
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase
 // https://firebase.google.com/docs/web/setup#available-libraries
 // import { initializeApp } from 'firebase/app';
 // import { getDatabase, ref, set } from "firebase/database";
-import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-database.js";
+import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-database.js";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
